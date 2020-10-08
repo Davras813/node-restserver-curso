ad90fae52c6bf0fd8e47989e08bd7eb13df14d48
David <davidloaiza13@gmail.com>
2020-10-07 22:42:18 -0500

    Crear un usuario en BD

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const bodyParser = require('body-parser');
+//const app = require('./routes/usuario');
 
 
 // parse application/x-www-form-urlencoded
@@ -11,44 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario (espacio para prueba!!)')
+mongoose.connect('mongodb://localhost:27017/koffe', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario.'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-
-
-    res.json('post Usuario')
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
-
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto:', process.env.PORT);
