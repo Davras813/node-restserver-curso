e89a1b8fc9090662c8ab434ae3764d2a27fbe6a6
David <davidloaiza13@gmail.com>
2020-10-08 21:33:09 -0500

    Crear un usuario en BD

---
+++ server/models/usuario.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        //default: 'Llenar',
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        //default: false,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        default: '',
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        default: 'No',
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
